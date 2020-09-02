b918e4feb2b8d35cf0d05fed46639397208968e0
William Pedersen <William Pedersen@192.168.0.115>
2020-09-02 21:05:26 +0200

    Innlevering 1

---
+++ JavaInnlevering1/src/no/hvl/dat100/oppgave6.java
@@ -0,0 +1,17 @@
+package no.hvl.dat100;
+
+import static java.lang.Integer.parseInt;
+import static javax.swing.JOptionPane.showInputDialog;
+
+public class oppgave6 {
+	public static void main(String[] args) {
+
+		int fakultet = parseInt(showInputDialog("Skriv inn fakulteten: "));
+		long ganger = 1;
+		for (int i = 1; i <= fakultet; i++) {
+			ganger = ganger * i;
+
+		}
+		System.out.println("svaret blir: " + ganger);
+	}
+}
