bc7fb03ecc12bf0a0f2b24aee258871f041ac459
williampedersen <williampedersen@192.168.0.92>
2020-09-07 14:03:35 +0200

    tester igjen

---
+++ src/no/hvl/dat100/kulevolum.java
@@ -0,0 +1,18 @@
+package no.hvl.dat100;
+import static java.lang.Math.*;
+import static java.lang.Integer.*;
+import static javax.swing.JOptionPane.*;
+
+
+public class kulevolum {
+
+	public static void main(String[] args) {
+		
+		double radius = parseInt(showInputDialog("skriv inn radius"));
+		double volum = (4.0/3.0)*PI*pow(radius,3);
+		
+System.out.println(volum);
+		
+	}
+
+}
